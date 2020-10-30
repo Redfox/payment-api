69ba25ea5e6499a2bf639896e9da79e5ce3e3ade
VitorRedfox <vitor_pereira.2013@hotmail.com>
2020-10-30 14:34:33 -0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+
+import express from 'express';
+import cors from 'cors';
+import { Server } from '@overnightjs/core';
+
+export default class App extends Server {
+  private server: express.Application;
+
+  constructor() {
+    super();
+    this.server = this.app;
+
+    this.server.use(cors());
+  }
+
+  async start(): Promise<void> {
+    this.server.listen(3333, () => {
+      console.log('🚀 Server ready at 3333');
+    })
+  }
+}
